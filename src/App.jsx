22679c128af862a006d903fe52a9877f4663d2be
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2025-11-09 12:48:01 +0600

    initial Implement project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+      <h1 className='text-2xl text-center my-6 text-red-500 font-bold'>Vite + React</h1>
+    
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <button class="btn btn-primary text-center mx-auto">Button</button>
+
+    </>
+  )
+}
+
+export default App
